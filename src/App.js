8a5b21f0743580c20b796c6fe4fc4c2eb631dc5f
Milko Lorinkov <milkolori@gmail.com>
2020-07-23 16:24:41 +0300

    emais obfuscated

--- src/App.js
+++ src/App.js
@@ -8,106 +8,105 @@ import CaregiversList from './CaregiversList';
 import CaregiverForm from './CaregiverForm';
 
 const CAREGIVERS_DATA = [
-    {
-      "id": 1094,
-      "firstName": "Caregiver",
-      "lastName": "20200721053305",
-      "email": "cg20200721053305@test.panth.com",
-      "state": "VA",
-      "zip": "22201",
-      "agencyName": "RCI",
-      //"coach": "Coach OFC"
-    },
-    {
-      "id": 1085,
-      "firstName": "Bertha",
-      "lastName": "Mejia",
-      "email": "mariabertha_mejia@yahoo.com",
-      "state": "TX",
-      "zip": "78542",
-      "agencyName": "RCI",
-      "coach": "Mildred Aranda"
-    },
-    {
-      "id": 1078,
-      "firstName": "Julissa",
-      "lastName": "Rubio",
-      "email": "rubio_julissa@yahoo.com",
-      "state": "NM",
-      "zip": "88007",
-      "agencyName": "RCI",
-      "coach": "Mildred Aranda"
-    },
-    {
-      "id": 1079,
-      "firstName": "Angelica",
-      "lastName": "Gasca",
-      "email": "agasca12@yahoo.com",
-      "state": "TX",
-      "zip": "79932",
-      "agencyName": "RCI",
-      "coach": "Mildred Aranda"
-    },
-    {
-      "id": 1082,
-      "firstName": "Destiny",
-      "lastName": "Gutshall",
-      "email": "destinygutshall@icloud.com",
-      "state": "VA",
-      "zip": "24445",
-      "agencyName": "RCI",
-      "coach": "Deidre Blascyk"
-    },
-    {
-      "id": 1068,
-      "firstName": "Bobbie Jo",
-      "lastName": "Merrill",
-      "email": "fearlesszen@zoho.com",
-      "state": "PA",
-      "zip": "16922",
-      "agencyName": "RCI",
-      "coach": "Deidre Blascyk"
-    },
-    {
-      "id": 1071,
-      "firstName": "Jaimeisha",
-      "lastName": "Johnson",
-      "email": "jaimeishaK@gmail.com",
-      "state": "VA",
-      "zip": "23513",
-      "agencyName": "RCI",
-      "coach": "Mildred Aranda"
-    },
-    {
-      "id": 1072,
-      "firstName": "Leila",
-      "lastName": "Raher",
-      "email": "leilabadii@gmail.com",
-      "state": "CA",
-      "zip": "92129",
-      "agencyName": "RCI",
-      "coach": "Britania Grajeda Esparza"
-    },
-    {
-      "id": 1074,
-      "firstName": "Emilia",
-      "lastName": "Nogic",
-      "email": "emilia.nogic19@gmail.com",
-      "state": "TX",
-      "zip": "75050",
-      "agencyName": "RCI",
-      "coach": "Deidre Blascyk"
-    },
-    {
-      "id": 1075,
-      "firstName": "Toni",
-      "lastName": "Parker-Wilson",
-      "email": "onboarddistricts@gmail.com",
-      "state": "TX",
-      "zip": "75040",
-      "agencyName": "RCI",
-      "coach": "Deidre Blascyk"
-    }
+  {
+    "id": 1094,
+    "firstName": "Caregiver",
+    "lastName": "20200721053305",
+    "email": "tes1@example.com.com",
+    "state": "VA",
+    "zip": "22201",
+    "agencyName": "RCI",
+  },
+  {
+    "id": 1085,
+    "firstName": "Bertha",
+    "lastName": "Mejia",
+    "email": "tes2@example.com",
+    "state": "TX",
+    "zip": "78542",
+    "agencyName": "RCI",
+    "coach": "Mildred Aranda"
+  },
+  {
+    "id": 1078,
+    "firstName": "Julissa",
+    "lastName": "Rubio",
+    "email": "tes3@example.com",
+    "state": "NM",
+    "zip": "88007",
+    "agencyName": "RCI",
+    "coach": "Mildred Aranda"
+  },
+  {
+    "id": 1079,
+    "firstName": "Angelica",
+    "lastName": "Gasca",
+    "email": "tes3@example.com",
+    "state": "TX",
+    "zip": "79932",
+    "agencyName": "RCI",
+    "coach": "Mildred Aranda"
+  },
+  {
+    "id": 1082,
+    "firstName": "Destiny",
+    "lastName": "Gutshall",
+    "email": "tes4@example.com",
+    "state": "VA",
+    "zip": "24445",
+    "agencyName": "RCI",
+    "coach": "Deidre Blascyk"
+  },
+  {
+    "id": 1068,
+    "firstName": "Bobbie Jo",
+    "lastName": "Merrill",
+    "email": "tes5@example.com",
+    "state": "PA",
+    "zip": "16922",
+    "agencyName": "RCI",
+    "coach": "Deidre Blascyk"
+  },
+  {
+    "id": 1071,
+    "firstName": "Jaimeisha",
+    "lastName": "Johnson",
+    "email": "test6@example.com",
+    "state": "VA",
+    "zip": "23513",
+    "agencyName": "RCI",
+    "coach": "Mildred Aranda"
+  },
+  {
+    "id": 1072,
+    "firstName": "Leila",
+    "lastName": "Raher",
+    "email": "test7@example.com",
+    "state": "CA",
+    "zip": "92129",
+    "agencyName": "RCI",
+    "coach": "Britania Grajeda Esparza"
+  },
+  {
+    "id": 1074,
+    "firstName": "Emilia",
+    "lastName": "Nogic",
+    "email": "test.tes1@example.com",
+    "state": "TX",
+    "zip": "75050",
+    "agencyName": "RCI",
+    "coach": "Deidre Blascyk"
+  },
+  {
+    "id": 1075,
+    "firstName": "Toni",
+    "lastName": "Parker-Wilson",
+    "email": "tes8@example.com",
+    "state": "TX",
+    "zip": "75040",
+    "agencyName": "RCI",
+    "coach": "Deidre Blascyk"
+  }
 ];
 
 const COACHES_DATA = [
